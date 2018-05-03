84056fe51977c6f58d9de473966e01779ce71583
sinlov <sinlovgmppt@gmail.com>
2018-05-03 10:03:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# this is my Ruby PlayGround
+
+# env
+
+|item|version|desc|
+|---|---|---|
+|ruby|2.3.0p0|2.3.0p0 (2015-12-25 revision 53290) [x86_64-darwin14]|
+
---
+++ task/timeTask.rb
@@ -0,0 +1,26 @@
+=begin
+  file timeTask.rb
+  create "sinlov" on 16/11/25.
+  this program under the term of Ruby's License  
+  (http://www.ruby-lang.org/LINCENSE.txt) 
+=end
+
+
+# gem install 'rufus-scheduler'
+
+require 'rufus/scheduler'
+scheduler = Rufus::Scheduler.new
+puts Time.new
+puts 'start task time'
+scheduler.in '1s' do
+  puts Time.new
+  puts 'Time task running'
+end
+
+scheduler.every("2s") do
+  puts Time.now
+end
+
+# class MyTmeTask
+#
+# end
