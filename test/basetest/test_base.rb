d524855b9fe265f65f78973ef4d9a4c38353b794
sinlov <sinlovppt@gmail.com>
2020-12-26 18:16:24 +0800

    add test_base.rb show base name

--- test/basetest/test_base.rb
+++ test/basetest/test_base.rb
@@ -13,6 +13,7 @@ class BaseTest < MiniTest::Test
   def test_sleep
     # assert_equal exp, act, msg
     assert_equal "zzZ", @t_zzz, "I don't sleep well "
+    puts "@t_name '#{@t_name}'"
     refute_nil @t_name ,'faker name is empty'
     # assert_not_nil is just an alias for refute_nil, but it's Rails-only, not part of standard Minitest.
     assert @t_name != nil, 'faker name is empty'
